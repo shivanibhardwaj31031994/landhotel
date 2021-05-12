0b05ef966125725c7e6fcfcd950359996eb20fbc
shivanibhardwaj31031994 <66326499+shivanibhardwaj31031994@users.noreply.github.com>
2021-05-12 11:12:00 +0530

    updating boiler plate

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learned React - Now become an expert.
         </a>
       </header>
     </div>
